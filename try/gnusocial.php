5ad1cd4ecb61689ca57cc1e834d65135d3446273
DalmeGNU <dalmemail@gmail.com>
2019-07-15 14:27:54 +0200

    Add fossforward.com to server list

--- try/gnusocial.php
+++ try/gnusocial.php
@@ -1,5 +1,5 @@
 <?php
-	$nodos = array("https://loadaverage.org", "https://gnusocial.cc", "https://gnusocial.no", "https://gnusocial.net", "https://quitter.im");
+	$nodos = array("https://loadaverage.org", "https://gnusocial.cc", "https://gnusocial.no", "https://gnusocial.net", "https://quitter.im", "https://fossforward.com");
 	$n = array_rand($nodos, 1);
 	$url = $nodos[$n];
 	header('Location: '.$url);
