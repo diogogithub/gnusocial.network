ae619559dd0992ed9f9cd58e4d185fdee45f3e5d
Diogo Cordeiro <diogo@fc.up.pt>
2019-08-19 02:21:35 +0100

    Delete no longer relevant files from older website

--- try/gnusocial.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$nodos = array("https://loadaverage.org", "https://gnusocial.cc", "https://gnusocial.no", "https://gnusocial.net", "https://quitter.im", "https://fossforward.com");
-	$n = array_rand($nodos, 1);
-	$url = $nodos[$n];
-	header('Location: '.$url);
-	exit();
-?>
